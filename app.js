8b8a2478c19d282a54f1b0d928725c5e603e1391
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-07-04 18:20:50 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-setInterval(() => {
-  document.getElementById("temp").innerText = (20 + Math.random()*5).toFixed(1);
-  document.getElementById("hum").innerText = (40 + Math.random()*30).toFixed(1);
-}, 2000);
